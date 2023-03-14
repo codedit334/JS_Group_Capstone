551f27e3d4834ef3ce19c0c68c969eaf5dd1c826
ilyass atlassi <atlassiilyass@gmail.com>
2023-03-14 21:09:32 +0100

    Add script 2

--- src/modules/render-list.js
+++ src/modules/render-list.js
@@ -9,10 +9,8 @@ const renderData = (arr) => {
           <span class="title-wrapper">${obj.show.name} <i class="fa-regular fa-heart"></i></span>
           <span class="likes">5 likes</span>
           <button class="comments" onclick="activePopUp(${obj});">Comments</button>
-          
           <button class="reservations">Reservations</button>
-          </li>`;
-    console.log(obj.show);
+          </li>`
   });
 };
 
@@ -21,11 +19,18 @@ const details = document.querySelector('.details');
 const pop = document.querySelector('.pop-body');
 console.log(pop)
 
-window.activePopUp = (index) => {
+window.activePopUp = async (index) => {
+ console.log(index)
+
  details.style.display = 'flex'
+ let arr = getData();
+ arr = await arr;
+ console.log(arr)
+ const arrUNeed = arr.find(obj => +obj.show.id === +index);
+ console.log(arrUNeed);
   pop.innerHTML = `<button class="delete-popup" onclick="closeDetails()" type="button">
   </button>
-  <img  alt='poster' />
+  <img src="${arrUNeed.show.image.medium}" alt='poster' />
   <div class="summary">
   ljhihihihh
   '${index.show}'
