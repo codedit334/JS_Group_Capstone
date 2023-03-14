cafc6ad226cde7b63f798377d9f83aa0712a74c8
codedit334 <oussama7elabdioui@gmail.com>
2023-03-14 10:49:53 +0100

    Display data dynamically from API

---
+++ src/modules/render-list.js
@@ -0,0 +1,24 @@
+const renderData = (arr) => {
+  const list = document.querySelector('.items-list');
+
+  list.innerHTML = '';
+  arr.forEach((obj) => {
+    list.innerHTML += `<li> 
+          <img src='${obj.show.image.medium}' alt='poster' />
+          <span class="title-wrapper">${obj.show.name} <i class="fa-regular fa-heart"></i></span>
+          <span class="likes">5 likes</span>
+          <button class="comments">Comments</button>
+          <button class="reservations">Reservations</button>
+          </li>`;
+  });
+};
+
+const getData = async () => {
+  const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
+  const myJson = await response.json();
+
+  const result = myJson.slice(2, 8);
+  renderData(result);
+};
+
+getData();
