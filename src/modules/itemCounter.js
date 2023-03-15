8ceddb317171f9e2946727a1e474432878a11b68
codedit334 <oussama7elabdioui@gmail.com>
2023-03-15 15:24:36 +0100

    Add items counter feature

---
+++ src/modules/itemCounter.js
@@ -0,0 +1,6 @@
+const itemsCounter = (el) => {
+  const elements = document.querySelectorAll(el);
+  return elements.length;
+};
+
+export default itemsCounter;
--- src/modules/render-list.js
+++ src/modules/render-list.js
@@ -1,49 +1,62 @@
 // import { activePopUp } from "./pop-up.js";
 
+import itemsCounter from './itemCounter.js';
+
+const addLike = () => {
+  document.querySelectorAll('.likeBtn').forEach((element) => {
+    const tvID = element.closest('li').getAttribute('data-id');
+    element.addEventListener('click', async () => {
+      const plusLikesResponse = await fetch(
+        'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4aTSjUAMwHbqF73Z3l5i/likes',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            item_id: tvID,
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        },
+      );
+      getData(); // eslint-disable-line no-use-before-define
+      return plusLikesResponse;
+    });
+  });
+};
+
 const renderData = (arr) => {
-  const list = document.querySelector(".items-list");
-  list.innerHTML = "";
+  const list = document.querySelector('.items-list');
+  list.innerHTML = '';
   arr.forEach((obj) => {
     list.innerHTML += `<li data-id='${obj.show.id}'> 
           <img src='${obj.show.image.medium}' alt='poster' />
-          <span class="title-wrapper">${obj.show.name} <i class="fa-regular fa-heart"></i></span>
+          <span class="title-wrapper">${obj.show.name} <i class="fa-regular fa-heart likeBtn"></i></span>
           <span class="likes">${obj.likes} likes</span>
           <button class="comments" onclick="activePopUp(${obj.show.id});">Comments</button>
           <button class="reservations">Reservations</button>
           </li>`;
   });
+  document.querySelector('.count').innerHTML = itemsCounter('.items-list li');
+  addLike();
 };
 
 const getData = async () => {
-  const response = await fetch("https://api.tvmaze.com/search/shows?q=girls");
+  const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
   const myJson = await response.json();
 
-    // const plusLikesResponse = await fetch(
-    //   "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4aTSjUAMwHbqF73Z3l5i/likes",
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       item_id: "525",
-    //     }),
-    //     headers: {
-    //       "Content-type": "application/json; charset=UTF-8",
-    //     },
-    //   }
-    // );
-
   const likesResponse = await fetch(
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4aTSjUAMwHbqF73Z3l5i/likes"
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4aTSjUAMwHbqF73Z3l5i/likes',
   );
   const likesJson = await likesResponse.json();
-
-  console.log(likesJson);
 
   const result = myJson.slice(2, 8);
 
   result.forEach((tvObj) => {
+    tvObj.likes = 0;
+  });
+  result.forEach((tvObj) => {
     likesJson.forEach((likesObj) => {
       if (+tvObj.show.id === +likesObj.item_id) tvObj.likes = likesObj.likes;
-      else tvObj.likes = 0;
     });
   });
 
@@ -51,11 +64,11 @@ const getData = async () => {
   return result;
 };
 
-const details = document.querySelector(".details");
-const pop = document.querySelector(".pop-body");
+const details = document.querySelector('.details');
+const pop = document.querySelector('.pop-body');
 
 window.activePopUp = async (index) => {
-  details.style.display = "flex";
+  details.style.display = 'flex';
   let arr = getData();
   arr = await arr;
   const arrUNeed = arr.find((obj) => +obj.show.id === +index);
@@ -69,8 +82,8 @@ window.activePopUp = async (index) => {
   <div class="show-details-1">
         <span>Score: ${parseInt(arrUNeed.score * 100, 10)}%</span>
         <span>Network: ${
-          arrUNeed.show.network ? arrUNeed.show.network.name : "Unkown"
-        }</span>
+  arrUNeed.show.network ? arrUNeed.show.network.name : 'Unkown'
+}</span>
     </div>
     <div class="show-details-2">
         <span>Language:&nbsp;${arrUNeed.show.language}</span>
@@ -81,10 +94,11 @@ window.activePopUp = async (index) => {
 };
 
 window.closeDetails = () => {
-  details.style.display = "none";
+  details.style.display = 'none';
   //   body.style.overflow = 'auto';
 };
 
+// addLike();
 getData();
 
 export default getData();
