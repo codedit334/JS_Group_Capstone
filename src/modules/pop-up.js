69dfb1100b224d198e76ac98fd60230ba694d825
ilyass atlassi <atlassiilyass@gmail.com>
2023-03-15 13:16:05 +0100

    Add script 3

--- src/modules/pop-up.js
+++ src/modules/pop-up.js
@@ -2,11 +2,10 @@ import { getData } from "./render-list.js";
 const main = document.querySelector('.main-section');
 const list = document.querySelector('.items-list');
 const header = document.querySelector('.header');
-
 console.log(main)
 export const activePopUp = (id) => {
     
-    main.innerHTML = `<div class= "pop-body">
+    main.innerHTML += `<div class= "pop-body">
     <img src='${id.show.image.original}' alt='poster' />
 
     </div>`
