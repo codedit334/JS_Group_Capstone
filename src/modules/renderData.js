2b2a1e4894f4f51614bb08903e2e0cfa7accded5
codedit334 <oussama7elabdioui@gmail.com>
2023-03-15 19:10:35 +0100

    Add tests

--- src/modules/renderData.js
+++ src/modules/renderData.js
@@ -1,17 +1,23 @@
-import itemsCounter from './itemCounter.js';
+import itemsCounter from "./itemCounter.js";
 
 export default async function renderData(arr) {
   const ourArr = await arr;
-  const list = document.querySelector('.items-list');
-  list.innerHTML = '';
-  ourArr.forEach((obj) => {
-    list.innerHTML += `<li data-id='${obj.show.id}'> 
+  const list = document.querySelector(".items-list");
+  list.innerHTML = "";
+  if (ourArr.length > 0) {
+    // console.log(ourArr)
+    ourArr.forEach((obj) => {
+      list.innerHTML += `<li data-id='${obj.show.id || 0}'> 
             <img src='${obj.show.image.medium}' alt='poster' />
             <span class="title-wrapper">${obj.show.name} <i class="fa-regular fa-heart likeBtn" onClick="addLike(${obj.show.id})"></i></span>
             <span class="likes">${obj.likes} likes</span>
             <button class="comments" onclick="activePopUp(${obj.show.id});">Comments</button>
             <button class="reservations">Reservations</button>
             </li>`;
-  });
-  document.querySelector('.count').innerHTML = itemsCounter('.items-list li');
+    });
+    document.querySelector(".count").innerHTML = itemsCounter(".items-list li");
+  }
+  // console.log(itemsCounter(".items-list li"))
+  const count = itemsCounter(".items-list li");
+  return count;
 }
--- src/test/items_counter.test.js
+++ src/test/items_counter.test.js
@@ -1,6 +1,46 @@
-import itemsCounter from '../modules/itemCounter.js';
-import renderData from '../modules/renderData.js';
+import itemsCounter from "../modules/itemCounter.js";
+import renderData from "../modules/renderData.js";
 
-it('Store item in local storage', () => {
-  expect(1).not.toBeNull();
+const simulateList = () => {
+  const ul = document.createElement("ul");
+  ul.className = "items-list";
+  document.querySelector("body").appendChild(ul);
+};
+
+const simulateSpan = () => {
+  const span = document.createElement("span");
+  span.className = "count";
+  document.querySelector("body").appendChild(span);
+};
+
+beforeAll(() => {
+  simulateList();
+  simulateSpan();
+});
+
+describe("Items Count", () => {
+  it("No items count", () => {
+    let arr = [];
+
+    renderData(arr);
+
+    expect(itemsCounter(".items-list li")).toBe(0);
+  });
+
+  it("Full Items Count", async () => {
+    let arr = [
+      {
+        show: { id: 0, image: { medium: "src" }, name: "name" },
+        likes: 0,
+      },
+      {
+        show: { id: 0, image: { medium: "src" }, name: "name" },
+        likes: 0,
+      },
+    ];
+
+    await renderData(arr);
+
+    expect(itemsCounter("ul li")).toBe(2);
+  });
 });
