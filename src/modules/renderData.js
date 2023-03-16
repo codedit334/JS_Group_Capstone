5d57431e7c1e2d0503916bace0aebd93fb8156d8
codedit334 <oussama7elabdioui@gmail.com>
2023-03-16 19:08:45 +0100

    Final styling

--- src/modules/renderData.js
+++ src/modules/renderData.js
@@ -11,7 +11,6 @@ export default async function renderData(arr) {
             <span class="title-wrapper">${obj.show.name} <i class="fa-regular fa-heart likeBtn" onClick="addLike(${obj.show.id})"></i></span>
             <span class="likes">${obj.likes} likes</span>
             <button class="comments" onclick="activePopUp(${obj.show.id});">Comments</button>
-            <button class="reservations">Reservations</button>
             </li>`;
     });
     document.querySelector('.count').innerHTML = itemsCounter('.items-list li');
