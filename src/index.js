730a249cc882c605afc84e3080f6719d28c12024
codedit334 <oussama7elabdioui@gmail.com>
2023-03-16 20:00:04 +0100

    Switch style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "./styles/index.css";
-import "./modules/getData.js";
-import "./modules/pop-up.js";
-import "./modules/renderData.js";
-import "./modules/addLike.js";
-import "./modules/windowFunctions.js";
-import "./modules/fetchComment.js";
+import './styles/index.css';
+import './modules/getData.js';
+import './modules/pop-up.js';
+import './modules/renderData.js';
+import './modules/addLike.js';
+import './modules/windowFunctions.js';
+import './modules/fetchComment.js';
--- src/modules/addLike.js
+++ src/modules/addLike.js
@@ -1,18 +1,18 @@
-import { getData } from "./getData.js";
-import renderData from "./renderData.js";
+import { getData } from './getData.js';
+import renderData from './renderData.js';
 
 window.addLike = async (tvID) => {
   const response = await fetch(
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4aTSjUAMwHbqF73Z3l5i/likes",
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4aTSjUAMwHbqF73Z3l5i/likes',
     {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         item_id: tvID,
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
-    }
+    },
   );
 
   const dataArr = getData();
