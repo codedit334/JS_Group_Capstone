ebbc8bbef49135a3dac194837adb66d9ce92833f
codedit334 <oussama7elabdioui@gmail.com>
2023-03-16 22:04:50 +0100

    Fix missing function

--- src/modules/list.js
+++ src/modules/list.js
@@ -1,11 +1,11 @@
-import commentsCounter from "./commentCount";
+import commentsCounter from './commentCount';
 
 export const listComment = (e, comsec) => {
   const temp = `${e.creation_date} ${e.username} : ${e.comment}`;
-  const per = document.createElement("li");
+  const per = document.createElement('li');
   per.innerHTML = temp;
   comsec.appendChild(per);
-  const header = document.querySelector(".head");
-  header.innerHTML = commentsCounter(".test li");
+  const header = document.querySelector('.head');
+  header.innerHTML = commentsCounter('.test li');
 };
 export default listComment;
