502e7368beec0d4a372d4422afed82b6b7fe87bb
codedit334 <oussama7elabdioui@gmail.com>
2023-03-17 00:40:47 +0100

    Add old modules

---
+++ src/modules/Addcomment.js
@@ -0,0 +1,19 @@
+/* eslint-disable  */
+export const addcomment = (dataid, name, text) => {
+
+  const add = async () => {
+    const res = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jSvbwvFa0M2a3HGwtKaG/comments',
+      {
+        method: 'post',
+        body: JSON.stringify({
+          item_id: dataid,
+          username: name,
+          comment: text,
+        }),
+        headers: { 'content-type': 'application/json' },
+      });
+    const data = await res.text();
+    return data;
+  };
+  add();
+};
---
+++ src/modules/commentCount.js
@@ -0,0 +1,6 @@
+const commentsCounter = (el) => {
+  const elements = document.querySelectorAll(el);
+  return elements.length;
+};
+
+export default commentsCounter;
