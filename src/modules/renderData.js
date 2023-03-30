a072189a6b5b11a36f8aaeb4a5c54eb9a0caa0e0
codedit334 <oussama7elabdioui@gmail.com>
2023-03-30 19:33:42 +0000

    Polish code

--- src/modules/renderData.js
+++ src/modules/renderData.js
@@ -9,7 +9,7 @@ export default async function renderData(arr) {
     ourArr.forEach((obj) => {
       list.innerHTML += `
         <li data-id='${obj.show.id || 0}'> 
-            <img src='${obj.show.image.medium}' alt='poster' />
+            <img src='${obj.show.image && obj.show.image.medium}' alt='poster' />
             <span class="title-wrapper">${obj.show.name} <i class="fa-regular fa-heart likeBtn" onClick="addLike(${obj.show.id})"></i></span>
             <span class="likes">${obj.likes} likes</span>
             <button class="comments" onclick="activePopUp(${obj.show.id});">Comments</button>
